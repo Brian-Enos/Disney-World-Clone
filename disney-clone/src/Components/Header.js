0f17700dda0d263ad48ea4166c8190de37930cf5
Brian-Enos <brianenos9199@gmail.com>
2023-06-22 17:01:27 +0300

    Added all the required images and videos for the project

--- disney-clone/src/Components/Header.js
+++ disney-clone/src/Components/Header.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className="Container">Header</div>
+    <div className="Nav">
+      {/* logo begins here */}
+      <div className='Logo'>
+        <img src="" />
+      </div>
+      {/*  logo ends here */}
+
+      {/* navmenu begins here */}
+      <div className='NavMenu'></div>
+      {/* navmenu ends here */}
+
+    </div>
   )
 }
 
