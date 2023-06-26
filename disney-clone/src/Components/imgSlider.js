1e76595327ed739f83f3bf720de6d4b6f1f30a11
Brian-Enos <brianenos9199@gmail.com>
2023-06-26 11:49:54 +0300

    Linked the second image file to the document

--- disney-clone/src/Components/imgSlider.js
+++ disney-clone/src/Components/imgSlider.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import Sliderbadging from '../Images/slider-badging.jpg';
+import Sliderbadag from '../Images/slider-badag.jpg';
 
-function imgSlider() {
-
-  let Settings = {
+function ImgSlider() {
+  let settings = {
     dots: true,
     infinite: true,
-    speed: 500, /* in milliseconds */
+    speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true
-  }
+  };
+
   return (
-    <div className='Carousel' {...Settings}>
+    <Slider {...settings} className='Carousel'>
       <div className='Wrap'>
-        <img src={Sliderbadging}  alt=""/>
+        <img src={Sliderbadging} alt='' />
       </div>
-    </div>
-  )
+      <div className='Wrap'>
+        <img src={Sliderbadag} alt='' />
+      </div>
+    </Slider>
+  );
 }
 
-export default imgSlider;
+export default ImgSlider;
