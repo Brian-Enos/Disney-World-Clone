cf539414e6cf8c1931e1c0371da46ffd127013dc
Brian-Enos <brianenos9199@gmail.com>
2023-06-26 11:01:06 +0300

    rendering home component

--- disney-clone/src/App.js
+++ disney-clone/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from "./Components/Header";
-import Home from "./Components/Home"
+import Home from "./Components/Home";
 
 function App() {
   return (
