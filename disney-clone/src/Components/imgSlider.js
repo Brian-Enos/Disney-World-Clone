dcc5c293e2649ed2d3da9d75c2048d94445262f7
Brian-Enos <brianenos9199@gmail.com>
2023-06-26 11:20:43 +0300

    Linked the first slider image to my workspace

--- disney-clone/src/Components/imgSlider.js
+++ disney-clone/src/Components/imgSlider.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Sliderbadging from '../Images/slider-badging.jpg';
 
 function imgSlider() {
+
+  let Settings = {
+    dots: true,
+    infinite: true,
+    speed: 500, /* in milliseconds */
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true
+  }
   return (
-    <div>imgSlider</div>
+    <div className='Carousel' {...Settings}>
+      <div className='Wrap'>
+        <img src={Sliderbadging}  alt=""/>
+      </div>
+    </div>
   )
 }
 
