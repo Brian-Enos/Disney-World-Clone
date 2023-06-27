ceac3c07934c40fbf57eff2b01bbbb82ba324534
Brian-Enos <brianenos9199@gmail.com>
2023-06-27 12:41:28 +0300

    The details component was declared to hold al the details required

--- disney-clone/src/Components/Detail.js
+++ disney-clone/src/Components/Detail.js
@@ -1,8 +1,35 @@
-import React from 'react'
-
+import React from 'react';
+import PlayIcon from "../Images/play-icon-black.png";
+import TrailerIcon from "../Images/play-icon-white.png";
+import GroupImage from "../Images/group-icon.png";
 function Detail() {
   return (
-    <div>Detail</div>
+    <div className='DetailContainer'>
+      <div className='Background'>
+        <img src="https://images2.alphacoders.com/444/444259.jpg" alt=""/>
+      </div>
+
+      <div className='ImageTitle'>
+        <img src="https://upload.wikimedia.org/wikipedia/en/0/0e/So_Random%21.png" alt="" />
+      </div>
+
+      <div className='Controls'>
+        <div className='PlayButton'>
+          <img src={PlayIcon} alt="" />
+          <span>PLAY</span>
+        </div>
+        <div className='TrailerButton'>
+          <img src={TrailerIcon} alt="" />
+          <span>TRAILER</span>
+        </div>
+        <div className='AddButton'>
+          <span>+</span>
+        </div>
+        <div className='GroupWatchButton'>
+          <img src={GroupImage} alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
 
