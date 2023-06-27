3b6e8a7950e754ee817f24e2258e04a5b2c273eb
Brian-Enos <brianenos9199@gmail.com>
2023-06-27 10:10:33 +0300

    Rendering four movie items to home.js

---
+++ disney-clone/src/Components/Movies.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Movies() {
+  return (
+    <div className='MovieContainer'>
+     <h4>Recommended for You</h4>
+     <div className='MovieContent'>
+        <div className='MoviewWrap'>
+          <img src="https://i.pinimg.com/736x/f8/24/c3/f824c3d1ea2bc7b2cbab2246adf591f6.jpg" alt=""/>
+        </div>
+
+        <div className='MoviewWrap'>
+          <img src="https://i.pinimg.com/736x/f8/24/c3/f824c3d1ea2bc7b2cbab2246adf591f6.jpg" alt=""/>
+        </div>
+
+        <div className='MoviewWrap'>
+          <img src="https://i.pinimg.com/736x/f8/24/c3/f824c3d1ea2bc7b2cbab2246adf591f6.jpg" alt=""/>
+
+        </div>
+
+        <div className='MoviewWrap'>
+          <img src="https://i.pinimg.com/736x/f8/24/c3/f824c3d1ea2bc7b2cbab2246adf591f6.jpg" alt=""/>
+        </div>
+
+        
+
+      </div>
+    </div>
+  )
+}
+
+export default Movies
