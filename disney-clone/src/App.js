c6cf7d3e7690ae90a24d85b121be8490ca487a46
Brian-Enos <brianenos9199@gmail.com>
2023-06-27 11:22:30 +0300

    Used react router to render the Home and Detail Sections as well as the Header element

--- disney-clone/src/App.js
+++ disney-clone/src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
+/* import { Counter } from './features/counter/Counter'; */
 import './App.css';
 import Header from "./Components/Header";
 import Home from "./Components/Home";
+import Detail from "./Components/Detail";
+
+import { BrowserRouter,
+  Routes,
+   Route,
+    /* Link */
+    } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/detail/" element={<Detail />}/>
+        </Routes>
+      </BrowserRouter>
+      
 
     </div>
   );
